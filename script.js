ec704da29e50ae60f412974997ae20c80bf5feb8
CyanNinja <68496747+Cyan-Ninja@users.noreply.github.com>
2020-09-04 09:50:08 +1000

    Fixed squash/stretch bug.

--- script.js
+++ script.js
@@ -15,17 +15,18 @@ function stepper() {
 }
 async function stepOne() { // Get The Camera & Display In Video
 	if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) { // Check For Media Devices
-		const videoStream = await navigator.mediaDevices.getUserMedia({video: true});
-		console.log(videoStream);
-		video.srcObject = videoStream;
+		//const videoStream = await navigator.mediaDevices.getUserMedia({video: true});
+		//console.log(videoStream);
+		//video.srcObject = videoStream;
+		video.src = "https://www.w3schools.com/html/mov_bbb.mp4";
 	} else {
 		console.error("No Media Devices!");
 	}
 }
 function stepTwo() { // Snap Photo
 	if (video.videoWidth > video.videoHeight) {
-		canvas.getContext("2d").drawImage(video, 0, 0, video.videoHeight + video.videoHeight / 2, video.videoHeight, 0, 0, canvas.width, canvas.height);
+		canvas.getContext("2d").drawImage(video, (video.videoWidth - video.videoHeight) / 2, 0, video.videoHeight, video.videoHeight, 0, 0, canvas.width, canvas.height);
 	} else {
-		canvas.getContext("2d").drawImage(video, 0, 0, video.videoWidth, video.videoWidth + video.videoWidth / 2, 0, 0, canvas.width, canvas.height);
+		canvas.getContext("2d").drawImage(video, 0, (video.videoHeight - video.videoWidth) / 2, video.videoWidth, video.videoWidth, 0, 0, canvas.width, canvas.height);
 	}
 }
