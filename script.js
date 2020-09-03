d3543e8d3625c456c636ae997d361fcf06620887
CyanNinja <68496747+Cyan-Ninja@users.noreply.github.com>
2020-09-04 09:11:00 +1000

    Promise works, working on snap.

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-var step = 0;
+var step = 0, video = document.querySelector("video"), canvas = document.querySelector("canvas");
 function stepper() {
 	step++;
 	switch (step) {
 		case 1:
 			stepOne();
 			break;
+		case 1:
+			stepTwo();
+			break;
 		default:
 		console.error("Incorrect Step: " + step);
+		stepTwo(); // TEMP: Just For Development
 	}
 }
 async function stepOne() { // Get The Camera & Display In Video
 	if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) { // Check For Media Devices
 		const videoStream = await navigator.mediaDevices.getUserMedia({video: true});
 		console.log(videoStream);
-		const video = document.querySelector("#video");
 		video.srcObject = videoStream;
 	} else {
 		console.error("No Media Devices!");
 	}
+}
+function stepTwo() { // Snap Photo
+	canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.width, 0, 0, canvas.width, canvas.width);
 }
