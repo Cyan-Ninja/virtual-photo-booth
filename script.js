82c161b5f1b51bc6d1dd2f42116042a29d558c3c
CyanNinja <68496747+Cyan-Ninja@users.noreply.github.com>
2020-09-04 08:53:22 +1000

    "Should" display camera feed as video.

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ function stepper() {
 		console.error("Incorrect Step: " + step);
 	}
 }
-function stepOne() { // Get The Camera
+function stepOne() { // Get The Camera & Display In Video
 	if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) { // Check For Media Devices
-		console.log(navigator.mediaDevices.getUserMedia({video: true}));
+		const videoStream = navigator.mediaDevices.getUserMedia({video: true});
+		console.log(videoStream);
+		const video = document.querySelector("#video");
+		video.srcObject = videoStream;
 	} else {
 		console.error("No Media Devices!");
 	}
