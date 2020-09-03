1d5e4aa2326bb42509b9e5a0cacb07c83b141d16
CyanNinja <68496747+Cyan-Ninja@users.noreply.github.com>
2020-09-04 09:52:01 +1000

    Commented video example (desktop doesn't have webcam). Examplar video courtesy of 'Big Buck Bunny' (old animated movie).

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ function stepper() {
 }
 async function stepOne() { // Get The Camera & Display In Video
 	if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) { // Check For Media Devices
-		//const videoStream = await navigator.mediaDevices.getUserMedia({video: true});
-		//console.log(videoStream);
-		//video.srcObject = videoStream;
-		video.src = "https://www.w3schools.com/html/mov_bbb.mp4";
+		const videoStream = await navigator.mediaDevices.getUserMedia({video: true});
+		console.log(videoStream);
+		video.srcObject = videoStream;
+		//video.src = "https://www.w3schools.com/html/mov_bbb.mp4"; // TEMP: Just For Development (Desktop Doesn't Have Webcam)
 	} else {
 		console.error("No Media Devices!");
 	}
