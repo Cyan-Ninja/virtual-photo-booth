f21bd222607c4efd8c313fab0b35d0401b1a3aad
CyanNinja <68496747+Cyan-Ninja@users.noreply.github.com>
2020-09-04 08:44:00 +1000

    Got base of first step.

---
+++ script.js
@@ -0,0 +1,14 @@
+var step = 0;
+function stepper() {
+	step++;
+	switch (step) {
+		case 1:
+			stepOne();
+			break;
+		default:
+		console.error("Incorrect Step: " + step);
+	}
+}
+function stepOne() { // Get The Camera
+
+}
